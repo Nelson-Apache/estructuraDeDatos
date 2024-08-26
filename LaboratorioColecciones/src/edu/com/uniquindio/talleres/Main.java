f454fb64d817d8fa71549c1f65ee674c8fb05c74
nelson.apache <molina123apache@gmal.com>
2024-08-25 21:04:39 -0500

    Creacion del proyecto Java Talleres

---
+++ LaboratorioColecciones/src/edu/com/uniquindio/talleres/Main.java
@@ -0,0 +1,11 @@
+package edu.com.uniquindio.talleres;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Prueba de que funciona");
+	}
+
+}
