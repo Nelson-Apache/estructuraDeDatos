55d8b57d00381113c338a46c59889bee51160002
nelson.apache <molina123apache@gmal.com>
2024-08-27 09:13:45 -0500

    se crear el ejercicio3

--- LaboratorioColecciones/src/edu/com/uniquindio/talleres/Estudiante.java
+++ LaboratorioColecciones/src/edu/com/uniquindio/talleres/Estudiante.java
@@ -1,5 +1,21 @@
 package edu.com.uniquindio.talleres;
 
-public class Estudiante {
+public class Estudiante extends Persona{
+	
+	private int matriculaEstudiante;
+
+	public Estudiante(String nombre, int edad, String genero, int matriculaEstudiante) {
+		super(nombre, edad, genero);
+		// TODO Auto-generated constructor stub
+		this.matriculaEstudiante = matriculaEstudiante;
+	}
+
+	public int getMatriculaEstudiante() {
+		return matriculaEstudiante;
+	}
+	
+	
+	
+	
 
 }
